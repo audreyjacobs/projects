2f7489fcac56aeddd49a658a326f1a1971d23f7e
audreyjacobs <104110690+audreyjacobs@users.noreply.github.com>
2025-10-23 17:47:32 -0600

    Add files via upload

---
+++ Behavior Chart/script.js
@@ -0,0 +1,17 @@
+   // Import the functions you need from the SDKs you need
+   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+   // TODO: Add SDKs for Firebase products that you want to use
+   // https://firebase.google.com/docs/web/setup#available-libraries
+ 
+   // Your web app's Firebase configuration
+   const firebaseConfig = {
+     apiKey: "AIzaSyCCqBV0Y5skN3W7H3leex9p4CvQIoqdLJE",
+     authDomain: "bekah-s-roster.firebaseapp.com",
+     projectId: "bekah-s-roster",
+     storageBucket: "bekah-s-roster.firebasestorage.app",
+     messagingSenderId: "115134763139",
+     appId: "1:115134763139:web:4a57ad54ad6de9ad498bb6"
+   };
+ 
+   // Initialize Firebase
+   const app = initializeApp(firebaseConfig);
